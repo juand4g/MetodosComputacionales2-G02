a4c9a6618c9f8aa400f51b0cf8045dff4cbb71a2
Yeiferson Camacho <yeiferkmac@gmail.com>
2025-03-17 14:53:16 -0500

    ...

--- Taller 4/Taller 4.py
+++ Taller 4/Taller 4.py
@@ -47,6 +47,50 @@ A_std = sqrt_pi * np.std(ratios) / (np.sqrt(len(samples)) * np.mean(ratios)**2)
 
 print(f"1.b) A = {A:.3f} ± {A_std:.3f}")
 
+#PUNTO 2 ----------------------------------------------------------------------------------------------------------------------------------------------
+
+# Definir parámetros
+D1 = D2 = .50  # cm
+lambda_ = 670e-9  # Longitud de onda en metros
+A = 0.4e-3  # Apertura en metros
+a = 0.1e-3  # Ancho de rendija en metros
+d = 0.1e-2  # Separación entre rendijas en metros
+N = 100000  # Número de muestras aleatorias
+
+# Generar muestras aleatorias dentro de la apertura
+x = np.random.uniform(-A/2, A/2, N)
+y = np.random.choice([-d/2, d/2], N) + np.random.uniform(-a/2, a/2, N)
+
+# Evaluar la integral de Fresnel en distintos valores de z
+z_values = np.linspace(-4e-3, 4e-3, 10000)  # Rango de z en metros
+I = []
+
+for z in z_values:
+    phase = (np.pi / lambda_) * (x**2 / D1) + (np.pi / lambda_) * ((x - y)**2 / D2) + (np.pi / lambda_) * ((z - y)**2 / D1)
+    integral = np.mean(np.exp(1j * phase))
+    I.append(np.abs(integral)**2)
+
+# Normalizar la intensidad
+I /= np.max(I)
+
+# Cálculo de la intensidad clásica
+theta = np.arctan(z_values / D2)
+I_classic = (np.cos((np.pi * d / lambda_) * np.sin(theta))**2) * (np.sinc(a * np.sin(theta) / lambda_)**2)
+I_classic /= np.max(I_classic)  # Normalizar
+
+
+# Graficar los resultados
+plt.figure(figsize=(8, 5))
+plt.plot(z_values, I, label='Difracción de Fresnel (Monte Carlo)')
+plt.plot(z_values, I_classic, label='Modelo clásico', linestyle='dashed', color='r')
+plt.xlabel('z (m)')
+plt.ylabel('Intensidad Normalizada')
+plt.title('Comparación entre Fresnel y Modelo Clásico')
+plt.legend()
+plt.grid()
+plt.savefig("2.pdf")
+
+
 #PUNTO 3 -----------------------------------------------------------------------------------------------------------------------------------------------
 # Parámetros del sistema
 N = 150
@@ -254,4 +298,4 @@ for i, txt in enumerate(porcentajes):
 
 # Mostrar el histograma
 plt.grid(True, axis='y', linestyle='--', alpha=0.7)
-plt.show()
+plt.savefig("4.pdf")
